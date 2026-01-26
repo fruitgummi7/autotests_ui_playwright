e43408e2686a3066e2a8b84d404fe9712b9ada03
fruitgummi7 <firyuza611@gmail.com>
2026-01-26 18:59:12 +0300

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -36,5 +36,3 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     context = browser.new_context(storage_state="browser-state.json")
     yield context.new_page()
     browser.close()
-
-
