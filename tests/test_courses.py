1e66db28faeffb1659dab4f880be2b161e531381
fruitgummi7 <firyuza611@gmail.com>
2026-02-07 14:22:29 +0300

    Знакомство с PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -8,14 +8,13 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0", min_score="0")
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image('C:/courses/autotests-ui/testdata/files/image.png')
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image('C:/courses/autotests-ui/testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright",
         estimated_time="2 weeks",
